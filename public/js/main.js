15adde21aafb1cb776e473fa0a2ad1084defaa53
Ranjan Datta <ranjan@Ranjans-MacBook-Pro.local>
2016-05-20 10:24:54 -0700

    Modal Trigger and script changes

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,23 @@
 (function() {
     'use strict';
 
-    window.onload = function() {
-        var overlay = document.getElementsByClassName('overlay')[0];
-        console.dir(overlay.classList);
+    var trigger = document.getElementById('trigger');
+    var close = document.getElementsByClassName('close')[0];
+    var overlay = document.getElementsByClassName('overlay')[0];
+    trigger.onclick = function() {
         overlay.classList.add('is-open');
-    }
+    };
+
+    close.onclick =  function(){
+	   overlay.classList.remove('is-open');
+    };
+
+    overlay.onclick = function(e)  {
+        if( e.target != this ) {
+            return;
+        }
+
+	   overlay.classList.remove('is-open');
+    };
 
 }());
